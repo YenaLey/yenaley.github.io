478178ff376a897f77f40dbc47d88ea84fd12c5f
YenaLey <yena.e121@gmail.com>
2024-02-18 04:21:02 +0900

    chore: project role 글자 축소

--- src/components/PROJECTS/PROJECTS.jsx
+++ src/components/PROJECTS/PROJECTS.jsx
@@ -15,7 +15,7 @@ function PROJECTS() {
         <div className="content-container">
 
           <Project2
-            role="웹 기획, 웹 디자인, 프론트엔드 개발"
+            role="웹 기획 및 디자인, 프론트엔드 개발"
             title="Portfolio Web"
             period="2024.02 - 2024.02"
             description="전체적인 기획과 디자인, 프론트엔드 개발까지 포괄하는 웹 개발 프로젝트입니다. 정보 구조(IA) 및 각 섹션별 상세 정보가 포함된 기획서를 작성하였으며, 다양한 포트폴리오 레퍼런스를 참고하여 디자인하였습니다. 이 웹사이트는 React.js를 기반으로 구축되었으며, 홈페이지의 PC 버전에서는 React Three Fiber를 사용하여 소개 멘트가 동적으로 변화하도록 구현하여 사용자 경험에 생동감을 더했습니다. 사용자 인터랙션에 부드러운 모션이 포함되어 있으며, 진부하지 않은 디자인을 목표로 하면서도 메시지의 전달력을 유지하는 데 중점을 두었습니다."
@@ -35,7 +35,7 @@ function PROJECTS() {
             notion="https://quipu-dev.notion.site/quipu-dev/main-website-c8661a4633804bfdaadb105c86d2df1e" /> */}
 
           <Project1
-            role="3D 그래픽스 & 인터랙티브 개발"
+            role="3D 그래픽스 프로그래밍"
             title="Three.js 기반 Luxo 램프 인터랙티브 시뮬레이션"
             period="2023.09 - 2023.10"
             description="Three.js을 활용하여 Luxo 램프의 인터랙티브 3D 시뮬레이션을 개발했습니다. 램프의 조절 가능한 부품들을 모델링하고, 스포트라이트 조명 효과를 추가하여 벽에 그림자를 생성하는 3D 모델을 장면에 배치했습니다. 사용자가 램프의 길이, 조인트 각도, 램프 각도를 쉽게 조절할 수 있는 GUI 제어 패널을 구현함으로써, 실시간으로 램프의 모양 변경이 가능하게 했습니다. 이를 통해, three.js 기반 웹 애플리케이션에서 사용자는 다양한 조명 및 그림자 효과를 경험할 수 있도록 개발하였습니다."
@@ -44,7 +44,7 @@ function PROJECTS() {
           />
 
           <Project2
-            role="아두이노 기반 회로 설계, 프로그래밍 및 데이터 처리, UI 개발"
+            role="아두이노 기반 회로 설계 및 프로그래밍"
             title="아두이노와 LTspice를 이용한 디지털 멀티미터"
             period="2023.03 - 2023.06"
             description="LTspice로 모델링된 회로와 아두이노를 사용하여 디지털 멀티미터를 개발하였습니다. 아두이노의 아날로그 입력을 통해 전압을 측정하고, Opamp 기반 차동 증폭기를 통해 측정값을 정확하게 변환합니다. 측정 가능한 범위 설정하여 구현하였고 단위 자동 변환 기능을 포함해, 저항, 전압, 전류, 축전기 용량을 측정하고 이를 I2C LCD에 표시하도록 설계하였습니다."
@@ -52,7 +52,7 @@ function PROJECTS() {
           />
 
           <Project1
-            role="FPGA 및 Verilog HDL을 사용한 설계와 구현 작업"
+            role="FPGA 및 Verilog HDL을 사용한 설계 및 구현"
             title="XILINX FPGA를 활용한 수학 연산 계산기"
             period="2022.09 - 2022.12"
             description="XILINX 기반 FPGA를 활용하여 Verilog HDL 기반의 계산기 설계하였습니다. 덧셈, 뺄셈, 곱셈, 나눗셈, 거듭제곱, 팩토리얼의 기능을 포함하며, 사용자는 Push Button Switch로 숫자를 입력하고 Dip Switch로 연산자, 피연산자의 순서, 음수 표현을 설정할 수 있습니다. 계산 결과는 Clock 기반의 TEXT LCD, 음수 표현은 LED를 통해 표시되도록 하였습니다. 7-Segment Array와 PIEZO를 사용하여 사용자 인터페이스와 피드백 메커니즘을 구현했으며, Vivado 프로그램을 통한 설계와 시뮬레이션을 거쳐 최종적으로 FPGA 디지털 회로에서의 시연으로 프로젝트를 완성했습니다."

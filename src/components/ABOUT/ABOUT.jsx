bdb91728479685cebf9d5f01d52ef2b29932a578
YenaLey <yena.e121@gmail.com>
2024-03-23 02:04:19 +0900

    feat: about 내용 수정

--- src/components/ABOUT/ABOUT.jsx
+++ src/components/ABOUT/ABOUT.jsx
@@ -23,7 +23,7 @@ function ABOUT() {
             <h2>Front-End 개발자</h2>
             <p>말보다 행동으로 먼저 나아가며, 꿈을 현실로 만들어내는 추진력이 강한 개발자입니다. 인터랙티브 디자인에 큰 관심을 가지고 있으며, 사용자에게 매력적이면서도 실용적인 인터랙션 경험을 제공하기 위해 끊임없이 노력하고 있습니다. 프론트엔드 개발에 주력하고 있으며, 팀 내 원활한 협업과 직접적인 의견 반영을 위해 기획, 디자인, 백엔드 분야에도 지속적으로 관심을 가지고 학습하고 있습니다.</p>
             <div className="about-content--bottom">
-              <p>Address: </p><span>서울특별시 동대문구 휘경동</span>
+              <p>Address: </p><span>서울시립대학교 학생회관 342호</span>
               <p>Email: </p><span>yena.e121@gmail.com</span>
               <p>Instagram: </p><span><a href="https://www.instagram.com/2ye._na/" target="_blank">2ye._na</a></span>
               <p>Github: </p><span><a href="https://github.com/YenaLey" target="_blank">YenaLey</a></span>
@@ -52,7 +52,7 @@ function ABOUT() {
         <div className="blank-container"></div>
         <Count countNum={180} title="coding days" />
         <Count countNum={5} title="projects completed" />
-        <Count countNum={326} title="github contributions" />
+        <Count countNum={386} title="github contributions" />
         <div className="blank-container"></div>
       </div>
       {/* RESUME */}

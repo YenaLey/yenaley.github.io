83677f3a882890dec547b2d0084d840b1e3b9601
YenaLey <yena.e121@gmail.com>
2024-08-30 14:52:56 +0900

    Feat: project 내용 업데이트

--- src/components/PROJECTS/PROJECTS.jsx
+++ src/components/PROJECTS/PROJECTS.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Element } from "react-scroll";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import "./PROJECTS.css";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { IoMdArrowRoundForward } from "react-icons/io";
 
 function PROJECTS() {
   return (
@@ -18,8 +20,10 @@ function PROJECTS() {
             <div></div>
             <p>
               서울시립대학교 컴퓨터 학술 중앙동아리에서 웹개발 팀을 결성하여,
-              다양한 <span style={{ fontWeight: "900" }}>도전</span>을 하고
-              있습니다.
+              <span style={{ fontWeight: "900" }}>
+                &nbsp;새로운 도전과 성취
+              </span>
+              를 이루고 있습니다.
             </p>
           </div>
 
@@ -30,8 +34,9 @@ function PROJECTS() {
             part="전체적인 기획과 디자인, 그리고 프론트엔드 개발을 담당하였습니다. GitHub Actions를 활용하여 CI/CD 파이프라인을 구축 및 관리하여 배포 프로세스를 자동화하고 있습니다."
             skillList={["Figma", "React.js", "Three.js", "GitHub"]}
             img={[
-              "./img/project/퀴푸메인웹.png",
-              "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+              "./img/project/퀴푸메인웹1.png",
+              "./img/project/퀴푸메인웹2.png",
+              "./img/project/퀴푸메인웹3.png",
             ]}
             link="https://quipu.uos.ac.kr/"
             figma="https://www.figma.com/design/Is4NkQmdc6bAGJrkVLzomp/QUIPU-DEV?node-id=584-2&node-type=CANVAS&t=wCakjTO9wYDqt2Wf-0"
@@ -39,12 +44,12 @@ function PROJECTS() {
           />
 
           <Project
-            title="QUIPU Event"
+            title="QUIPU Roulette Event"
             period="2024.08"
-            goal="동아리 소개 및 부원 모집 시스템 구축 그리고 다양한 이벤트를 진행하며 동아리를 홍보하고자 하였습니다. 웹 개발 작업물들에 대한 피드백에 따른 History를 담는 프로젝트 쇼케이스를 포함합니다."
-            part="전체적인 기획과 디자인, 그리고 프론트엔드 개발을 담당하였습니다. GitHub Actions를 활용하여 CI/CD 파이프라인을 구축 및 관리하여 배포 프로세스를 자동화하고 있습니다."
-            skillList={["Figma", "React.js", "Three.js", "GitHub"]}
-            img={["./img/project/퀴푸메인웹.png"]}
+            goal="서울시립대학교 학부생들을 대상으로 동아리를 홍보하기 위해 랜덤 선물 뽑기 이벤트를 진행했습니다."
+            part="이벤트의 전체 기획과 디자인, 프론트엔드 개발을 담당했습니다. 중복 참여와 의도적인 결과 화면 접속으로 인한 문제를 방지하고, 튕김 발생 시 같은 결과 화면에 재접속할 수 있도록 하는 등 많은 사람들이 참여하며 발생할 수 있는 에러들을 방지하고자 하였습니다."
+            skillList={["Figma", "React.js", "GitHub"]}
+            img={["./img/project/룰렛이벤트.mp4"]}
             figma="https://www.figma.com/design/Is4NkQmdc6bAGJrkVLzomp/QUIPU-DEV?node-id=668-1947&node-type=CANVAS&t=mxF0DUvbQts55V13-0"
             github="https://github.com/Quipu-Developers/main-frontend"
             reverse
@@ -53,10 +58,13 @@ function PROJECTS() {
           <Project
             title="QUIPU BackOffice Web"
             period="2024.07 - 2024.08"
-            goal="동아리 소개 및 부원 모집 시스템 구축 그리고 다양한 이벤트를 진행하며 동아리를 홍보하고자 하였습니다. 웹 개발 작업물들에 대한 피드백에 따른 History를 담는 프로젝트 쇼케이스를 포함합니다."
-            part="전체적인 기획과 디자인, 그리고 프론트엔드 개발을 담당하였습니다. GitHub Actions를 활용하여 CI/CD 파이프라인을 구축 및 관리하여 배포 프로세스를 자동화하고 있습니다."
+            goal="동아리 지원 정보를 실시간으로 편리하게 모니터링하기 위해 동아리 어드민 웹페이지를 개발했습니다."
+            part="전체적인 기획과 프론트엔드 개발을 담당했습니다. 퀴푸 메인 웹의 지원 폼을 통해 제출된 내용을 실시간으로 불러오며, 단축키를 통해 특정 내용 복사 및 모달 컨트롤을 효율적으로 할 수 있습니다. 지원자의 이름, 학번, 모집 동기, 포트폴리오 PDF 등의 정보를 포함하며, PDF 제목을 클릭하면 해당 파일을 요청 후 로컬로 다운로드할 수 있습니다. 또한, 실시간으로 불러온 데이터를 Excel 파일로 다운로드할 수 있습니다."
             skillList={["React.js", "GitHub"]}
-            img={["./img/project/퀴푸메인웹.png"]}
+            img={[
+              "./img/project/퀴푸백오피스1.png",
+              "./img/project/퀴푸백오피스2.png",
+            ]}
             link="https://quipu-developers.github.io/backoffice-frontend"
             github="https://github.com/Quipu-Developers/backoffice-frontend"
           />
@@ -64,9 +72,9 @@ function PROJECTS() {
           <Project
             title="LingoConnect"
             period="2024.07 - 2024.08"
-            goal="동아리 소개 및 부원 모집 시스템 구축 그리고 다양한 이벤트를 진행하며 동아리를 홍보하고자 하였습니다. 웹 개발 작업물들에 대한 피드백에 따른 History를 담는 프로젝트 쇼케이스를 포함합니다."
-            part="전체적인 기획과 디자인, 그리고 프론트엔드 개발을 담당하였습니다. GitHub Actions를 활용하여 CI/CD 파이프라인을 구축 및 관리하여 배포 프로세스를 자동화하고 있습니다."
-            skillList={["Figma", "React.js", "GitHub"]}
+            goal="지적장애인을 위한 생성형 AI를 통해 현실감 있는 음성 대화 및 발음 분석과 피드백을 통한 언어학습을 지원하는 서비스입니다."
+            part="기획과 프론트엔드 개발을 담당하였습니다. REST API를 활용한 백엔드 연동을 주로 담당하였고, STT(음성 인식)와 TTS(텍스트 음성 변환) 기능을 구현했습니다. 클라이언트에서 녹음된 음성을 WAV 파일로 변환해 백엔드로 전송하고, 백엔드에서 받은 데이터를 실시간으로 처리하여 사용자 인터페이스에 반영했습니다."
+            skillList={["React.js", "GitHub"]}
             img={[
               "./img/project/링고커넥트1.mp4",
               "./img/project/링고커넥트2.mp4",
@@ -89,9 +97,9 @@ function PROJECTS() {
 
           <Project
             title="Portfolio Web"
-            period="2024.02 - 2024.02"
-            goal="저를 소개하기 위한 웹 페이지입니다. 피드백을 반영하며 퍼블리싱 실력 변화를 비교하는 것을 목적으로 하였습니다."
-            part="전반적인 기획과 디자인 그리고 프론트엔드를 개발하였습니다."
+            period="2024.02 -"
+            goal="개발 공부를 하며 나아가고자 하는 방향성을 표현하고, 참여한 프로젝트들에 대한 기록을 남기기 위한 개인 웹사이트를 제작하였습니다."
+            part="웹사이트의 전체 기획, 디자인, 그리고 프론트엔드 개발을 담당하였습니다. 사용자가 쉽게 탐색할 수 있도록 직관적인 UI/UX를 설계하고, 반응형 디자인을 적용하여 다양한 디바이스에서 최적의 경험을 제공하였습니다. 또한, 참여한 프로젝트를 효과적으로 보여줄 수 있는 기능들을 구현하여, 개발자로서의 성장과 성과를 기록할 수 있는 플랫폼을 구현하고자 하였습니다."
             skillList={["React.js", "Three.js"]}
             img={["./img/project/포트폴리오1.png"]}
             link="https://yena.info/"
@@ -102,11 +110,12 @@ function PROJECTS() {
           <Project
             title="Luxo 램프 인터랙티브 시뮬레이션"
             period="2023.09 - 2023.10"
-            goal="Three.js를 활용하여 인터랙티브한 시뮬레이션 기능을 적용시켜 보는 것을 목적으로 하였습니다."
-            part="시뮬레이션 기능에 대한 기획과 구현을 하였습니다"
-            skillList={["Three.js"]}
+            goal="Three.js에 입문하기 위한 프로젝트로, Luxo 램프의 인터랙티브한 시뮬레이션 기능을 구현하였습니다."
+            part="시뮬레이션 기능의 기획과 전체 구현을 담당하였습니다. Luxo 램프의 각 부분을 모듈화하여 3D 모델링하고, GUI를 통해 램프의 관절과 조명을 제어할 수 있는 기능을 구현했습니다. 또한, 사용자 입력에 따라 실시간으로 변화를 반영하는 렌더링과 조명 효과를 포함한 물리 기반의 시뮬레이션을 구현하였습니다."
+            skillList={["Three.js", "Vanilla JS"]}
             img={["./img/project/컴퓨터그래픽스프젝2.png"]}
-            link="https://yenaley.github.io/computer-graphics/"
+            link="https://yenaley.github.io/Luxo-Interactive-Simulation/"
+            github="https://github.com/YenaLey/Luxo-Interactive-Simulation"
           />
         </div>
         <div className="blank-container"></div>
@@ -207,20 +216,30 @@ function Project({
   link,
   figma,
   github,
-  notion,
   reverse,
 }) {
   const [imgIndex, setImgIndex] = useState(0);
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (!inView) {
+      setImgIndex(0);
+    }
+  }, [inView]);
 
   const renderMedia = (src, index) => {
     const fileExtension = src.split(".").pop().toLowerCase();
+    const activeClass = index === imgIndex ? "active" : "";
     if (
       fileExtension === "mp4" ||
       fileExtension === "webm" ||
       fileExtension === "ogg"
     ) {
       return (
-        <video key={index} controls>
+        <video key={index} className={activeClass} controls>
           <source src={src} type={`video/${fileExtension}`} />
           <track
             src="path/to/captions.vtt"
@@ -233,19 +252,29 @@ function Project({
         </video>
       );
     } else {
-      return <img key={index} src={src} alt="프로젝트 사진" />;
+      return (
+        <img
+          key={index}
+          src={src}
+          alt="프로젝트 사진"
+          className={activeClass}
+        />
+      );
     }
   };
 
   return (
-    <div className={reverse ? "project-block" : "project-block-reverse"}>
+    <div
+      ref={ref}
+      className={reverse ? "project-block" : "project-block-reverse"}
+    >
       <div className="laptop">
         {imgIndex > 0 && (
           <div
             className="project-img-button"
             onClick={() => setImgIndex(imgIndex - 1)}
           >
-            &#60;
+            <IoMdArrowRoundBack />
           </div>
         )}
         {imgIndex < img.length - 1 && (
@@ -253,10 +282,12 @@ function Project({
             className="project-img-button right"
             onClick={() => setImgIndex(imgIndex + 1)}
           >
-            &#62;
+            <IoMdArrowRoundForward />
           </div>
         )}
-        <div className="laptop-img">{renderMedia(img[imgIndex], imgIndex)}</div>
+        <div className="laptop-img">
+          {img.map((src, index) => renderMedia(src, index))}
+        </div>
         <div className="keyboard">
           <div className="keyboard-top"></div>
           <div className="keyboard-bottom"></div>
@@ -283,13 +314,11 @@ function Project({
         </p>
         <div className="project-skill-container">
           {skillList &&
-            skillList.map(function (skill, index) {
-              return (
-                <div key={index} className="project-skill">
-                  {skill}
-                </div>
-              );
-            })}
+            skillList.map((skill, index) => (
+              <div key={index} className="project-skill">
+                {skill}
+              </div>
+            ))}
         </div>
         <div className="icon-container">
           {link && (
@@ -313,19 +342,11 @@ function Project({
               </div>
             </a>
           )}
-          {notion && (
-            <a href={notion} target="_blank" rel="noopener noreferrer">
-              <div className="icon">
-                <img src="./img/icon/기록아이콘.png" alt="Notion Icon"></img>
-              </div>
-            </a>
-          )}
         </div>
       </div>
     </div>
   );
 }
-
 function SideProject(props) {
   const { ref, inView } = useInView({
     triggerOnce: true,
